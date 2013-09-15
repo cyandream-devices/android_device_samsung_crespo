a178f8c79177221d6537fee3857ae1965a808415
yanniks <kontakt@yanniks.de>
2013-09-15 12:58:52 +0200

    CyanDream compatibility

--- cm.mk
+++ cm.mk
@@ -1,15 +1,15 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Inherit some common CD stuff.
+$(call inherit-product, vendor/cyandream/config/gsm.mk)
 
 PRODUCT_RELEASE_NAME := NS
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common CD stuff.
+$(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/crespo/full_crespo.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cyandream/config/nfc_enhanced.mk)
 
 PRODUCT_NAME := cm_crespo
 PRODUCT_DEVICE := crespo
